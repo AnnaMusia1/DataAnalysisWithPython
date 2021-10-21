176852a57aadb8558afc81736dea091055290ca4
AnnaMusia1 <annkierepka@gmail.com>
2021-10-21 13:19:41 +0200

    Importing and Exporting Data in Python

--- importing_and_exporting_data.py
+++ importing_and_exporting_data.py
@@ -1,1 +1,19 @@
 import pandas as pd
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
+
+# our data has no headers, so we need to specify it
+df = pd.read_csv(url, header=None)
+
+# print(df)
+print(df.head(3))
+
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels",
+           "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-power", "num-of-cylinders",
+           "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg",
+           "highway-mpg", "price"]
+df.columns = headers
+print(df.head(3))
+
+path = "C:/Users/a.musial/IdeaProjects/DataAnalysisWithPython/automobile.csv"
+df.to_csv(path)
